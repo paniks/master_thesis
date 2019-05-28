0863b4768477feaf40bd3261fcc8973fa5682da0
xavier sulkowski <xavier.sulkowski@lekta.ai>
2019-05-29 00:22:28 +0200

    sending jsons to app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, redirect, request
 
 project = 'keystroke_dynamics'
 
 app = Flask(__name__,  template_folder=f'web/{project}/view', static_folder=f'web/{project}/utils')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'True'
 
 
 @app.route("/")
-def keystroke_app():
+def index():
     return render_template('welcome.html')
+
+
+@app.route("/save", methods=["POST", "GET"])
+def save():
+    data = request.form['strikes']
+    json_list = json.loads(data)
+    return redirect('/')
 
 
 if __name__ == "__main__":
--- web/keystroke_dynamics/utils/keylogger.js
+++ web/keystroke_dynamics/utils/keylogger.js
@@ -1,11 +1,24 @@
 var outputField = document.getElementById("timestamps")
+var agregatedData = [];
 function logKeyEvent(){
     var now = new Date()
     entry = {"sourceid": event.srcElement.attributes.id.nodeValue, "type": event.type, "time": now.getTime(),
-             "key": event.key, "fieldvalue": event.srcElement.value};
+             "key": event.key};
     outputField.value += JSON.stringify(entry) + "\n";
     outputField.scrollTop = outputField.scrollHeight;
+    agregatedData.push(entry)
 }
 
 $('input').keydown(logKeyEvent);
 $('input').keyup(logKeyEvent);
+
+$( "#login" ).submit(function( event ) {
+  url = '/save'
+  data = { 'strikes': JSON.stringify(agregatedData)}
+  $.post(url, data, function(data, status){
+    console.log(`response status:  ${status}`)
+    $("#login")[0].reset()
+  })
+  event.preventDefault();
+});
+
